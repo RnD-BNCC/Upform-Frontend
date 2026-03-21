54c4e2f304bf283b8fdda628e09d8dcf170fa5c5
Yanzz231 <mrpartys05@gmail.com>
2026-03-21 18:08:11 +0700

    feat: add Pagination component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,4 +3,5 @@ export { default as GoogleIcon } from "./GoogleIcon";
 export { default as LoadingModal } from "./LoadingModal";
 export { default as LoadingScreen } from "./LoadingScreen";
 export { default as NoFormsIllustration } from "./NoFormsIllustration";
+export { default as Pagination } from "./Pagination";
 export { default as StatusModal } from "./StatusModal";
