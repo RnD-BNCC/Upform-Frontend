a2f0fd35e85054a11720ab28ece88cb8823184da
Yanzz231 <mrpartys05@gmail.com>
2026-05-07 12:41:47 +0700

    feat: export GalleryContextMenu and GalleryShareModal

--- src/pages/gallery/components/index.ts
+++ src/pages/gallery/components/index.ts
@@ -1,5 +1,7 @@
 export { default as FileCard } from "./FileCard";
 export { default as FolderItem } from "./FolderItem";
+export { default as GalleryContextMenu } from "./GalleryContextMenu";
 export { default as GalleryHero } from "./GalleryHero";
+export { default as GalleryShareModal } from "./GalleryShareModal";
 export { default as ImagePreviewModal } from "./ImagePreviewModal";
 export { default as MediaCard } from "./MediaCard";
