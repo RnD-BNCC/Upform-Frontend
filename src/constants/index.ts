5be4b2232464c44748d621df21d230da01cfe93c
Yanzz231 <mrpartys05@gmail.com>
2026-06-06 23:50:03 +0700

    refactor: rename appearance constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export * from "@/constants/api";
 export * from "@/constants/brand";
-export * from "@/constants/builderPageAppearance";
+export * from "@/constants/appearance";
 export * from "@/constants/countries";
 
